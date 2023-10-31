f4986a160539ddfa1621372ec5b3543f611af769
Thomas Tran <thomas.tran@hostplus.com.au>
2023-11-01 06:46:16 +1100

    Add project files.
    Signed-off-by: Thomas Tran <tldinteractive@gmail.com>

---
+++ Owf.Sd.Jwt/SDConstants.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Immutable;
+
+namespace Owf.Sd.Jwt;
+/// <summary>
+/// A class that contains constants used in the SD-JWT specification.
+/// </summary>
+public static class SDConstants
+{
+    /// <summary>
+    /// The default hash algorithm used in SD-JWT.
+    /// </summary>
+    public const string DEFAULT_HASH_ALGORITHM = "sha-256";
+
+    /// <summary>
+    /// The "_sd" key reserved by the SD-JWT specification.
+    /// </summary>
+    public const string KEY_SD = "_sd";
+
+    /// <summary>
+    /// The "_sd_alg" key reserved by the SD-JWT specification.
+    /// </summary>
+    public const string KEY_SD_ALG = "_sd_alg";
+
+    /// <summary>
+    /// The "_sd_jwt" key reserved by the SD-JWT specification.
+    /// </summary>
+    public const string KEY_SD_JWT = "_sd_jwt";
+
+    /// <summary>
+    /// The "..." key reserved by the SD-JWT specification.
+    /// </summary>
+    public const string KEY_THREE_DOTS = "...";
+
+    /// <summary>
+    /// Keys reserved by the SD-JWT specification.
+    /// </summary>
+    public static readonly ImmutableHashSet<string> RESERVED_KEYS = ImmutableHashSet.Create
+    (
+        KEY_SD,
+        KEY_SD_ALG,
+        KEY_SD_JWT,
+        KEY_THREE_DOTS
+    );
+
+    /// <summary>
+    /// Claims that are not selectively-disclosable according to the SD-JWT specification.
+    /// </summary>
+    public static readonly ImmutableHashSet<string> RETAINED_CLAIMS = ImmutableHashSet.Create
+    (
+        "iss",
+        "iat",
+        "nbf",
+        "exp",
+        "cnf",
+        "type",
+        "status"
+    );
+}
---
+++ Owf.Sd.Jwt/SDUtilities.cs
@@ -0,0 +1,48 @@
+﻿using gfoidl.Base64;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Owf.Sd.Jwt;
+
+public static class SDUtilities
+{
+    public static byte[] ComputeDigest(HashAlgorithm algorithm, byte[] data)
+    {
+        return algorithm.ComputeHash(data);
+    }
+
+    public static string ComputeDigest(HashAlgorithm algorithm, string data)
+    {
+        byte[] digest = ComputeDigest(algorithm, Encoding.UTF8.GetBytes(data));
+
+        return Base64.Url.Encode(digest);
+    }
+
+    public static string GenerateSalt()
+    {
+        return Convert.ToBase64String(RandomNumberGenerator.GetBytes(16));
+    }
+
+    public static string ToBase64Url(string input)
+    {
+        // Convert the input string to a byte array.
+        var bytes = Encoding.UTF8.GetBytes(input);
+
+        return ToBase64Url(bytes);
+    }
+
+    public static string ToBase64Url(byte[] input)
+    {
+        return Base64.Url.Encode(input);
+    }
+
+    public static string FromBase64Url(string base64Url)
+    {
+        return Encoding.UTF8.GetString(Base64.Url.Decode(base64Url));
+    }
+
+    public static bool IsReservedKey(string key)
+    {
+        return SDConstants.RESERVED_KEYS.Contains(key);
+    }
+}

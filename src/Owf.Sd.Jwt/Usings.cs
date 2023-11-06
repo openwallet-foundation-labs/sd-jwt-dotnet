e18f66c4ff009420853b1af4b0e43d2c43865312
Thomas Tran <thomas.tran@hostplus.com.au>
2023-11-07 10:12:26 +1100

    refactor
    Signed-off-by: Thomas Tran <thomas.tran@hostplus.com.au>

--- src/Owf.Sd.Jwt/Usings.cs
+++ src/Owf.Sd.Jwt/Usings.cs
@@ -1,1 +1,5 @@
-﻿
+﻿global using System.Collections.Immutable;
+global using System.Security.Cryptography;
+global using System.Text.Encodings.Web;
+global using System.Text.Json;
+global using System.Text.Unicode;

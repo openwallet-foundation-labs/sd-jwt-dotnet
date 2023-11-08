4af00fcf448b3f5131f2d745151cccf97633dc5e
Thomas Tran <tldinteractive@gmail.com>
2023-11-09 08:16:49 +1100

    added issuer
    Signed-off-by: Thomas Tran <tldinteractive@gmail.com>

--- src/Owf.Sd.Jwt.Examples/Issuer.cs
+++ src/Owf.Sd.Jwt.Examples/Issuer.cs
@@ -7,17 +7,6 @@ public class Issuer : IIssuer
 {
     public string Issue(Dictionary<string, object> claims, string issuerJwk)
     {
-        // Create object builder
-        ObjectBuilder builder = new();
-
-        // Create a disclosure for each claims
-        foreach (var claim in claims)
-        {
-            builder.AddDisclosure(Disclosure.Create(claim.Key, claim.Value));
-        }
-
-        var payload = builder.Build();
-
         // Prepare the header part of a credential JWT.
         // The header represents {"alg":"ES256","typ":"vc+sd-jwt"}.
         var headers = new Dictionary<string, object>()
@@ -28,7 +17,7 @@ public class Issuer : IIssuer
 
         var privateKey = Jwk.FromJson(issuerJwk, JWT.DefaultSettings.JsonMapper);
 
-        var token = Jose.JWT.Encode(payload, privateKey, JwsAlgorithm.ES256, extraHeaders: headers);
+        var token = Jose.JWT.Encode(claims, privateKey, JwsAlgorithm.ES256, extraHeaders: headers);
 
         return token;
     }
